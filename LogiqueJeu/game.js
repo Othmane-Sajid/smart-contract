8c4ee6bc54d360bb6790b4c2396970cb302edff6
Richard Gingras <richard.gingras1@videotron.ca>
2022-07-08 19:19:26 -0400

    Game retourne l'array des joueurs

--- LogiqueJeu/game.js
+++ LogiqueJeu/game.js
@@ -5,10 +5,9 @@ class Game{
     play(p1, p2){
         
         let players = new Array(p1, p2);
-        var bankrupt = false;
         var turn = players[0].choice();
 
-        while (bankrupt==false){
+        while (!(players[0].isBankrupt() || players[1].isBankrupt())){
         
             var player_bet = players[(turn + 1) % 2].choice();
             var player_flip = players[turn % 2].flip();
@@ -20,14 +19,11 @@ class Game{
                 players[(turn + 1) % 2].substractOnePennie();
                 players[turn % 2].addOnePennie();
             }
-   
-            if (players[0].isBankrupt() || players[1].isBankrupt()){
-                bankrupt = true;
-            }
             
             turn += 1;           
-        }       
-        return new Array(players[0].getPennies(), players[1].getPennies());
+        }   
+        return players;
+        
     }
 }
 
