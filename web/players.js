80d20c5b3e4fbbd29266a260291133fbe2307e17
Richard Gingras <richard.gingras1@videotron.ca>
2022-07-13 08:58:39 -0400

    pour jouer il faut des joueurs et une partie

---
+++ web/players.js
@@ -0,0 +1,39 @@
+Player = class Player{
+        
+    constructor(address, pennies){
+        this.address = address;
+        this.pennies = pennies;
+    }
+
+    addOnePennie(){
+        this.pennies += 1;
+    }
+
+    substractOnePennie(){
+        this.pennies -= 1;
+    }
+
+    isBankrupt(){
+        return this.pennies == 0;
+    }
+
+    getPennies(){
+        return this.pennies;
+    }
+
+    choice(){
+        let min = Math.ceil(0);
+        let max = Math.floor(100);
+        return Math.floor(Math.random() * (max - min + 1)) % 2;
+    }
+
+    flip(){
+        return this.choice();
+    }
+
+    getAddress(){
+        return this.address;
+    }
+}
+
+
